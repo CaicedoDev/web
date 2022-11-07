b8acc958d9189f2602a6c09b41c43760a3144a31
Pedro Caicedo <caicedo1089@gmail.com>
2022-11-07 14:44:41 -0500

    Fix

---
+++ main.js
@@ -0,0 +1,9 @@
+
+  var typed = new Typed('#display', {
+    stringsElement: '#words',
+    typeSpeed: 200,
+    backSpeed: 100,
+    cursorChar: '_',
+    smartBackspace: false,
+    loop: true
+  });
